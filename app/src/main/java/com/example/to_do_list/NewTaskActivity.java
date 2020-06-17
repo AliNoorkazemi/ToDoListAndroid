af4c988b18b36bd00ee29507bbf9518f2c9f0b84
AliNoorkazemi <alinoorkazemi@gmail.com>
2020-06-17 23:33:13 +0430

    date picker added.

--- app/src/main/java/com/example/to_do_list/NewTaskActivity.java
+++ app/src/main/java/com/example/to_do_list/NewTaskActivity.java
@@ -1,15 +1,22 @@
 package com.example.to_do_list;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class NewTaskActivity extends AppCompatActivity {
+
+    private DatePickerDialog datePicker;
     @Override
     public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +25,25 @@ public class NewTaskActivity extends AppCompatActivity {
         final EditText titleEdit = findViewById(R.id.titleEdit);
         final EditText descriptionEdit = findViewById(R.id.descriptionEdit);
         final EditText expirationDateEdit = findViewById(R.id.ExpirationDateEdit);
+        expirationDateEdit.setInputType(InputType.TYPE_NULL);
+        expirationDateEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                datePicker = new DatePickerDialog(NewTaskActivity.this , new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        expirationDateEdit.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                    }
+                },year, month, day);
+
+                datePicker.show();
+            }
+        });
 
         Button checkButton = findViewById(R.id.checkButton);
         checkButton.setOnClickListener(new View.OnClickListener() {
